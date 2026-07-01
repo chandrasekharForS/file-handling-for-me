1cdee1f19cae84e8398b3af282dbab9cd299732b
Chandrasekhar <chandru.p.b.cms@gmail.com>
2026-07-01 11:24:44 +0530

    second save of important program to explain threads

---
+++ src/com/files/demo/PersonCRUD.java
@@ -0,0 +1,44 @@
+package com.files.demo;
+
+import java.util.List;
+
+public class PersonCRUD {
+	private static final String FILE_NAME = "persons.ser";
+	
+	// Load people from the file
+	public static List<Person> loadPeople() {
+		return null;
+	}
+	
+	// Save people to the file
+	public static void savePeople(List<Person> people) {
+		
+	}
+	
+    // Add a new person
+	public static void addPerson(Person person) {
+		
+	}
+    
+	// Read all people
+	public static void readAllPeople() {
+		
+	}
+	
+	// Update a person's details by name
+	public static void updatePerson(String name, Person newDetails) {
+		
+	}
+	
+    // Delete a person by name
+	public static void deletePerson(String name) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
